9f1e0ae2b1ab9a6fd96178b886959e35df98103a
ziqiYang <919460668@qq.com>
2018-11-28 10:54:39 +0800

    first commit to create a project by springboot init

---
+++ src/main/java/cn/edu/dhu/springbootgithub/SpringbootGithubApplication.java
@@ -0,0 +1,12 @@
+package cn.edu.dhu.springbootgithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGithubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootGithubApplication.class, args);
+    }
+}
